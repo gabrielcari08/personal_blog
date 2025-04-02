d6a5b0fa079c9d709c5edafb725f9eb48a71bf14
gabrielcari08 <gabrielcari2008@gmail.com>
2025-04-02 15:49:37 -0300

    primera version del personal blog terminada

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.hello, name="hello"), #URL de prueba
-    path('list_articles/', views.list_articles, name="list_articles")
+    path('', views.list_articles, name="list_articles"),
+    path('article/<int:article_id>/', views.article_detail, name="article_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Articles
 
 # Create your views here.
 
-def hello(request): #Vista de prueba
-    return HttpResponse("Hello World")
-
 def list_articles(request):
     articles = Articles.objects.all()
     return render(request, 'home.html', {"articles": articles})
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    return render(request, 'article_detail.html', {"article": article})
